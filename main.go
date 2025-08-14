9808afb4b2636f4fb89be489dcc53444dfac6899
guille <guille@users.noreply.github.com>
2025-08-14 19:33:54 +0200

    Add another site parser and switch to Goroutines arch

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 
 	"github.com/guille/rss-builder/rss"
+	"github.com/guille/rss-builder/sites/gabriel_albiac"
 	"github.com/guille/rss-builder/sites/rory_sutherland"
 )
 
@@ -17,6 +18,7 @@ type Parser interface {
 
 func main() {
 	parsers := []Parser{
+		gabriel_albiac.Parser{},
 		rory_sutherland.Parser{},
 	}
 
@@ -25,30 +27,52 @@ func main() {
 		log.Fatalf("error creating output dir: %v", err)
 	}
 
+	errCh := make(chan error, len(parsers))
+
 	for _, parser := range parsers {
-		items, err := parser.Fetch()
-		if err != nil {
-			log.Printf("error fetching %s: %v", parser.Name(), err)
-			continue
-		}
+		go func() {
+			items, err := parser.Fetch()
+			if err != nil {
+				log.Printf("error fetching %s: %v", parser.Name(), err)
+				errCh <- err
+				return
+			}
 
-		filename := filepath.Join(outputDir, parser.Name()+".xml")
-		f, err := os.Create(filename)
-		if err != nil {
-			log.Printf("error creating %s: %v", filename, err)
-			continue
-		}
-		defer f.Close()
+			filename := filepath.Join(outputDir, parser.Name()+".xml")
+			f, err := os.Create(filename)
+			if err != nil {
+				log.Printf("error creating %s: %v", filename, err)
+				errCh <- err
+				return
+			}
+			defer f.Close()
 
-		channel := rss.Channel{
-			Title:       parser.Name() + " feed",
-			Link:        parser.URL(),
-			Description: "Scraped feed for " + parser.Name(),
-			Items:       items,
-		}
+			channel := rss.Channel{
+				Title:       parser.Name() + " feed",
+				Link:        parser.URL(),
+				Description: "Scraped feed for " + parser.Name(),
+				Items:       items,
+			}
 
-		if err := rss.Write(f, channel); err != nil {
-			log.Printf("error writing %s: %v", filename, err)
+			if err := rss.Write(f, channel); err != nil {
+				log.Printf("error writing %s: %v", filename, err)
+				errCh <- err
+				return
+			}
+
+			errCh <- nil
+		}()
+	}
+
+	var anyErr bool
+
+	for range parsers {
+		if err := <-errCh; err != nil {
+			anyErr = true
 		}
+	}
+
+	if anyErr {
+		os.Exit(1)
 	}
 }
---
+++ sites/gabriel_albiac/gabriel_albiac.go
@@ -0,0 +1,107 @@
+package gabriel_albiac
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+
+	"github.com/guille/rss-builder/rss"
+)
+
+const (
+	baseURL    = "https://www.eldebate.com/autor/gabriel-albiac/"
+	dateFormat = "02/01/2006"
+)
+
+type Parser struct{}
+
+func (Parser) Name() string { return "Gabriel Albiac (El Debate)" }
+func (Parser) URL() string  { return baseURL }
+func (Parser) Fetch() ([]rss.Item, error) {
+	var httpClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", baseURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("create request: %w", err)
+	}
+	req.Header.Set("User-Agent", "rss-builder")
+
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("fetch eldebate: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status: %d", res.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("parse html: %w", err)
+	}
+
+	var (
+		items    []rss.Item
+		firstErr error
+	)
+
+	doc.Find("article.c-article").Each(func(i int, s *goquery.Selection) {
+		if firstErr != nil {
+			return
+		}
+
+		titleSel := s.Find(".c-article__title")
+		if titleSel.Length() == 0 {
+			firstErr = fmt.Errorf("missing title selector at index %d", i)
+			return
+		}
+		title := strings.TrimSpace(titleSel.Text())
+		if title == "" {
+			firstErr = fmt.Errorf("empty title at index %d", i)
+			return
+		}
+
+		dateSel := s.Find("div.date")
+		if dateSel.Length() == 0 {
+			firstErr = fmt.Errorf("missing date selector at index %d", i)
+			return
+		}
+		inputDate := strings.TrimSpace(dateSel.Text())
+		parsedDate, perr := time.Parse(dateFormat, inputDate)
+		if perr != nil {
+			firstErr = fmt.Errorf("parse date %q at index %d: %w", inputDate, i, perr)
+			return
+		}
+
+		linkSel := s.Find("a.page-link")
+		if linkSel.Length() == 0 {
+			firstErr = fmt.Errorf("missing link selector at index %d", i)
+			return
+		}
+		relativeLink, exists := linkSel.Attr("href")
+		if !exists || relativeLink == "" {
+			firstErr = fmt.Errorf("empty link at index %d", i)
+			return
+		}
+		link := fmt.Sprintf("https://www.eldebate.com%v", relativeLink)
+
+		items = append(items, rss.Item{
+			Title:       title,
+			Link:        link,
+			Description: "",
+			GUID:        rss.NewGUID(link),
+			PubDate:     parsedDate.Format(rss.PubDateFormat),
+		})
+	})
+
+	if firstErr != nil {
+		return nil, firstErr
+	}
+	return items, nil
+}
